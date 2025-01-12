6622913a9ab3221a9c9c2194e2354afb91bbf5ec
Satyam Seth <satyam1998.1998@gmail.com>
2025-01-12 14:22:44 +0530

    Use axios for search api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+
+  async function submitHandler(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault(); // Prevent form submission from reloading the page
+
+    try {
+      const response = await axios({
+        method: "get",
+        url: `https://saavn.dev/api/search?query=${query}`,
+      });
+
+      console.log(response.data.data); // Log the search results
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  function onQueryChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(e.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="query">Search</label>
+        <input type="text" id="query" value={query} onChange={onQueryChange} />
+        <button type="submit">Fetch Song</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
